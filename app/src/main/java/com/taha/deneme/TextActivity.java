2e541d14b7915c65b64195e0716207bb754db21e
tahabayi <tahabayi@gmail.com>
2017-01-13 19:47:40 +0300

    Pagination added

--- app/src/main/java/com/taha/deneme/TextActivity.java
+++ app/src/main/java/com/taha/deneme/TextActivity.java
@@ -3,11 +3,16 @@ package com.taha.deneme;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -17,7 +22,12 @@ import com.google.firebase.storage.StorageReference;
 
 public class TextActivity extends Activity {
 
-    private TextView tv;
+    private TextView mTextView;
+    private Pagination mPagination;
+    private CharSequence mText;
+    private int mCurrentIndex = 0;
+
+
 
     private String texturl;
 
@@ -25,28 +35,79 @@ public class TextActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.text_activity);
+        setContentView(R.layout.activity_pagination);
+
+
         Intent intent = getIntent();
         texturl = intent.getStringExtra("texturl");
-        Log.d("story",texturl);
 
-        tv = (TextView) findViewById(R.id.storytext);
-        changeFont(tv);
+        mTextView = (TextView) findViewById(R.id.tv);
+        changeFont(mTextView);
         final FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storytext = storage.getReferenceFromUrl(texturl);
 
         final long ONE_MEGABYTE = 1024 * 1024;
-        storytext.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        Task t=storytext.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
             public void onSuccess(byte[] bytes) {
-                String text = new String(bytes);
-                tv.setText(text);
+                final String s = new String(bytes);
+                deneme(s);
             }
         });
+
+
+
+
+
+    }
+
+    private void update() {
+        final CharSequence text = mPagination.get(mCurrentIndex);
+        if(text != null) mTextView.setText(text);
     }
 
     protected void changeFont(TextView tv){
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Bookerly-Regular.ttf");
         tv.setTypeface(custom_font);
+    }
+    private void deneme(String s){
+        mText = s;
+        mPagination = new Pagination(mText,
+                mTextView.getWidth(),
+                mTextView.getHeight(),
+                mTextView.getPaint(),
+                mTextView.getLineSpacingMultiplier(),
+                mTextView.getLineSpacingExtra(),
+                mTextView.getIncludeFontPadding());
+        update();
+
+
+        /*mTextView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Removing layout listener to avoid multiple calls
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    mTextView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    mTextView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+
+            }
+        });*/
+
+        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex > 0) ? mCurrentIndex - 1 : 0;
+                update();
+            }
+        });
+        findViewById(R.id.btn_forward).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex < mPagination.size() - 1) ? mCurrentIndex + 1 : mPagination.size() - 1;
+                update();
+            }
+        });
     }
 }
